dc964e2a5ab6f92e7dcf4d10082dae8b9e4bfd77
unknown <you@example.com>
2021-05-08 21:21:46 +0700

    Format

--- src/libconverter/url.cpp
+++ src/libconverter/url.cpp
@@ -1,26 +1,21 @@
 #include "libconverter/url.h"
 #include <cstring>
 #include <stdio.h>
-
 int onURL(char* in, char* out)
 {
     int i = 0, j = 0;
     char link_name[100], link_url[100], all[200];
-
     while (in[i] != '[') {
         i++;
     }
     i++;
-
     while (in[i] != ']' && in[i] != '\n' && in[i] != EOF && i < 99) {
         link_name[j] = in[i];
         i++;
         j++;
     }
-
     link_name[j] = '\0';
     i++;
-
     if (in[i] == '(') {
         j = 0;
         while (in[i + 1] != ')' && in[i] != '\n') {
@@ -28,7 +23,6 @@ int onURL(char* in, char* out)
             j++;
             i++;
         }
-
         link_url[j] = '\0';
     }
     else {
@@ -36,7 +30,6 @@ int onURL(char* in, char* out)
         strcat(out, all);
         return 1;
     }
-
     sprintf(all, "<a href=\"%s\">%s</a>\n", link_url, link_name);
     strcat(out, all);
     return 0;
